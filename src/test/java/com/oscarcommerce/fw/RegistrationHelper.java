9153c78252a4b88da42db7c60909cde8793b4d50
Anastasia <basteta@ukr.net>
2023-01-28 23:59:53 +0200

    add account.feature

--- src/test/java/com/oscarcommerce/fw/RegistrationHelper.java
+++ src/test/java/com/oscarcommerce/fw/RegistrationHelper.java
@@ -14,15 +14,15 @@ public class RegistrationHelper extends BaseHelper {
         type(By.id("id_registration-password2"), pwd);
     }
 
-    public void enterEmail(String email) {
+    public void enterRegistrationEmail(String email) {
         type(By.id("id_registration-email"), email);
     }
 
-    public void enterPassword(String pwd) {
+    public void enterRegistrationPassword(String pwd) {
         type(By.id("id_registration-password1"), pwd);
     }
 
-    public void confirmPassword(String pwd) {
+    public void confirmRegistrationPassword(String pwd) {
         type(By.id("id_registration-password2"), pwd);
     }
 
@@ -41,5 +41,14 @@ public class RegistrationHelper extends BaseHelper {
         waitForElementToBeClickable(By.id("logout_link"));
     }
 
+    public String hasPasswordErrorMsg() {
+        return hasText(By.xpath("//span[@class='error-block'][1]"));
+    }
+
+    public void logOutUser() {
+        click(By.id("logout_link"));
+    }
 }
+
+
 
