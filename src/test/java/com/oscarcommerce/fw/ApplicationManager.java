769eb145be5082679c3919b4f25ec68bfa6dd543
Anastasia <basteta@ukr.net>
2023-01-28 22:47:37 +0200

    add changes to account.feature

--- src/test/java/com/oscarcommerce/fw/ApplicationManager.java
+++ src/test/java/com/oscarcommerce/fw/ApplicationManager.java
@@ -32,6 +32,7 @@ public class ApplicationManager {
 
     public static final String PWD_RESET_PAGE_PATH = "/password-reset/";
     private static final String SCREENSHOT_FILE_NAME = "target/screenshots/$timestamp_screenshot.png";
+    private static final String ACCOUNT_PAGE_PATH = "/accounts/profile/";
 
     public static String defaultBaseURL = PropertiesLoader.loadProperty("defaultBaseURL");
     public static String defaultBrowser = PropertiesLoader.loadProperty("defaultBrowser");
@@ -183,10 +184,6 @@ public class ApplicationManager {
         webDriver.get(baseUrl);
     }
 
-    public void goToLoginPage() {
-        webDriver.get(baseUrl + LOGIN_PAGE_PATH);
-    }
-
     public void goToLoginAndRegistrationPage() {
         webDriver.get(baseUrl + LOGIN_AND_REGISTRATION_PAGE_PATH);
     }
@@ -195,10 +192,9 @@ public class ApplicationManager {
         webDriver.get(baseUrl + PWD_RESET_PAGE_PATH);
     }
 
-    public void goToRegistrationPage() {
-        webDriver.get(baseUrl + REGISTRATION_PAGE_PATH);
+    public void goToAccountPage() {
+        webDriver.get(baseUrl + ACCOUNT_PAGE_PATH);
     }
-
 
     public void goToBasketPage() {
         webDriver.get(baseUrl + BASKET_PAGE_PATH);
@@ -248,4 +244,5 @@ public class ApplicationManager {
     }
 
 
+   
 }
--- src/test/java/com/oscarcommerce/glue/Hooks.java
+++ src/test/java/com/oscarcommerce/glue/Hooks.java
@@ -2,6 +2,8 @@ package com.oscarcommerce.glue;
 
 import com.oscarcommerce.fw.ApplicationManager;
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 public class Hooks {
     protected static ApplicationManager app;
@@ -10,12 +12,54 @@ public class Hooks {
         Hooks.app = app;
     }
 
-//    @BeforeAll
-//    public static void beforeAll() {
-//
-//    }
+    String validUserEmail = "testemail@gmail.com";
+    String validUserPwd = "validPassword1!";
 
-    @After
+
+    @Before(value = "@requiresLogin")
+    public void loginUserBeforeScenario(Scenario scenario) {
+        app.goToLoginAndRegistrationPage();
+        app.getLoginHelper().enterLoginEmail(validUserEmail);
+        app.getLoginHelper().enterLoginPassword(validUserPwd);
+        app.getLoginHelper().clickLoginBtn();
+    }
+
+    @Before(value = "@requiresRegistrationAndLogin")
+    public void registerAndLoginUserBeforeScenario(Scenario scenario) {
+        app.goToLoginAndRegistrationPage();
+        app.getRegistrationHelper().enterUserRegistrationCredentials(validUserEmail, validUserPwd);
+        app.getRegistrationHelper().clickRegisterBtn();
+        System.out.println("User is logged in");
+    }
+
+    @Before(value = "@requiresRegistrationWithoutLogIn")
+    public void registerUserBeforeScenario(Scenario scenario) {
+        app.goToLoginAndRegistrationPage();
+        app.getRegistrationHelper().enterUserRegistrationCredentials(validUserEmail, validUserPwd);
+        app.getRegistrationHelper().clickRegisterBtn();
+        app.getRegistrationHelper().logOutUser();
+        System.out.println("User is registered");
+    }
+
+    @After(value = "@loginsAndDeletesUserAccount", order = 3)
+    public void loginAndDeleteUserAccount() {
+        app.goToLoginAndRegistrationPage();
+        app.getLoginHelper().enterLoginEmail(validUserEmail);
+        app.getLoginHelper().enterLoginPassword(validUserPwd);
+        app.getLoginHelper().clickLoginBtn();
+        app.goToAccountPage();
+        app.getAccountHelper().deleteUserAccount();
+        System.out.println("Account is deleted");
+    }
+
+    @After(value = "@deletesUserAccount", order = 2)
+    public void deleteUserAccount() {
+        app.goToAccountPage();
+        app.getAccountHelper().deleteUserAccount();
+        System.out.println("Account is deleted");
+    }
+
+    @After(order = 1)
     public void afterScenario() {
 //        app.takeScreenShot();
 //        //app.takeScreenshotWithScrollDown();
@@ -23,9 +67,22 @@ public class Hooks {
         System.out.println("After is called!");
     }
 
+
 //    @After("@browser and not @headless")
 //    public void doSomethingAfter(Scenario scenario){
 //        scenario.isFailed();
 //    }
+
+    //    @After(order = 1)
+//    public void takeScreenshot() {
+//        app.takeScreenShot();
+//        app.takeScreenshotWithScrollDown();
+//    }
+
+//    @After(order = 2)
+//    public void closeApp() {
+//        app.stopApp();
+//    }
+
 
 }
